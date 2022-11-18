7c8ccce5fc0cbb6d57a8f2a8713eb4bcc1eb9d48
Jari Kovalainen <itse@jarikovalainen.com>
2022-11-18 11:44:07 +0000

    Päivityksiä

---
+++ pages/authors/me.js
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Layout from '../../components/layout';
+export default function Me() {
+    return (
+
+        <Layout>
+
+            <Head>
+                <title>Jarppa</title>
+            </Head>
+
+            <h1 className="title">Jari Kovalainen &rarr;</h1>
+
+        </Layout>
+    );
+}
